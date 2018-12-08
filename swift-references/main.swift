d34c3a219b36a1ba85f2a2dcbe61dccb1e5d5990
Max Desiatov <max@desiatov.com>
2018-12-08 11:49:24 +0000

    Initial Commit

---
+++ swift-references/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-references
+//
+//  Created by Max Desiatov on 08/12/2018.
+//  Copyright © 2018 Max Desiatov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
